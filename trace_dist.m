9b73598506342eb9977b94396450029dadfe3590
George Knee <georgeknee@googlemail.com>
2017-12-07 15:49:32 +0000

    nicer indenting

--- trace_dist.m
+++ trace_dist.m
@@ -1,8 +1,8 @@
 function [ d ] = trace_dist( A,B )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-[U,S,V] = svd(A-B);
-d = 0.5*trace(S);
-
+    [U,S,V] = svd(A-B);
+    d = 0.5*trace(S);
+    % nicely indented
 end
 
